b8195cd7cea543a88ef0708885328d03f7bbe215
Ryn <klanderson12@luc.edu>
2020-01-20 13:10:01 -0600

    json read

--- client.java
+++ client.java
@@ -1,57 +1,54 @@
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
-import java.util.LinkedHashMap;
+import java.io.FileReader;
+import java.util.Iterator;
 import java.util.Map;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.*;
 
 public class client
 {
-    public static void main(String[] args) throws FileNotFoundException
+    public static void main(String[] args) throws Exception
     {
-        // creating JSONObject 
-        JSONObject jo = new JSONObject();
+        // parsing file "JSONExample.json"
+        Object obj = new JSONParser().parse(new FileReader("JSONExample.json"));
 
-        // putting data to JSONObject 
-        jo.put("firstName", "John");
-        jo.put("lastName", "Smith");
-        jo.put("age", 25);
+        // typecasting obj to JSONObject
+        JSONObject jo = (JSONObject) obj;
 
-        // for address data, first create LinkedHashMap 
-        Map m = new LinkedHashMap(4);
-        m.put("streetAddress", "21 2nd Street");
-        m.put("city", "New York");
-        m.put("state", "NY");
-        m.put("postalCode", 10021);
+        // getting firstName and lastName
+        String firstName = (String) jo.get("firstName");
+        String lastName = (String) jo.get("lastName");
 
-        // putting address to JSONObject 
-        jo.put("address", m);
+        System.out.println(firstName);
+        System.out.println(lastName);
 
-        // for phone numbers, first create JSONArray  
-        JSONArray ja = new JSONArray();
+        // getting age
+        long age = (long) jo.get("age");
+        System.out.println(age);
 
-        m = new LinkedHashMap(2);
-        m.put("type", "home");
-        m.put("number", "212 555-1234");
+        // getting address
+        Map address = ((Map)jo.get("address"));
 
-        // adding map to list 
-        ja.add(m);
+        // iterating address Map
+        Iterator<Map.Entry> itr1 = address.entrySet().iterator();
+        while (itr1.hasNext()) {
+            Map.Entry pair = itr1.next();
+            System.out.println(pair.getKey() + " : " + pair.getValue());
+        }
 
-        m = new LinkedHashMap(2);
-        m.put("type", "fax");
-        m.put("number", "212 555-1234");
+        // getting phoneNumbers
+        JSONArray ja = (JSONArray) jo.get("phoneNumbers");
 
-        // adding map to list 
-        ja.add(m);
+        // iterating phoneNumbers
+        Iterator itr2 = ja.iterator();
 
-        // putting phoneNumbers to JSONObject 
-        jo.put("phoneNumbers", ja);
-
-        // writing JSON to file:"JSONExample.json" in cwd 
-        PrintWriter pw = new PrintWriter("JSONExample.json");
-        pw.write(jo.toJSONString());
-
-        pw.flush();
-        pw.close();
+        while (itr2.hasNext())
+        {
+            itr1 = ((Map) itr2.next()).entrySet().iterator();
+            while (itr1.hasNext()) {
+                Map.Entry pair = itr1.next();
+                System.out.println(pair.getKey() + " : " + pair.getValue());
+            }
+        }
     }
-} 
+}
