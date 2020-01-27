d9b2b8e716f493415f828fab3ac1a19511b0ce75
Ryn <klanderson12@luc.edu>
2020-01-27 12:21:36 -0600

    created class to read quiz

---
+++ readQuiz.java
@@ -0,0 +1,12 @@
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.*;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.FileReader;
+import java.util.*;
+
+
+public class readQuiz {
+}
